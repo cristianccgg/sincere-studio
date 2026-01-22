489d979e67b8b4d1e485a2e5f286496e80124535
cristianccgg <cristianccggg@gmail.com>
2026-01-22 15:43:14 -0500

    Initial commit - Sincere Studio project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/images/navbar/logo.png";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
