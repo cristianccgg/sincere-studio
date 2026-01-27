d6dada28233f097c41995fa6cee530b85c31fd15
cristianccgg <cristianccggg@gmail.com>
2026-01-26 20:22:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Landing from "./components/pages/Landing";
 
 function App() {
   return (
-    <div className="max-w-[1728px] mx-auto pl-[45px] pr-[36px] font-rajdhani">
+    <div className="max-w-[1728px] mx-auto md:pl-[45px] md:pr-[36px] px-5 font-rajdhani">
       <Navbar />
       <Landing />
     </div>
